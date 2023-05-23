bdc5191ef246c23dd8b06a448dca038475f7c264
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-05-23 10:41:32 +0200

    Project setup

---
+++ src/main/java/com/drossdrop/authservice/repository/UserCredentialRepository.java
@@ -0,0 +1,10 @@
+package com.drossdrop.authservice.repository;
+
+import com.drossdrop.authservice.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCredentialRepository  extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByName(String username);
+}
