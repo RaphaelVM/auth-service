d6d433bcffe23cf29310ec8424ec12c96940ed58
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-06-09 14:31:22 +0200

    Update AuthConfig.java

--- src/main/java/com/drossdrop/authservice/config/AuthConfig.java
+++ src/main/java/com/drossdrop/authservice/config/AuthConfig.java
@@ -26,7 +26,7 @@ public class AuthConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/register", "/api/auth/token").permitAll()
+                .requestMatchers("/api/auth/register", "/api/auth/token", "/api/auth/validate").permitAll()
                 .and()
                 .build();
     }
